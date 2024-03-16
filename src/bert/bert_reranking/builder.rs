4ccf34c76dfa14c12c56467a45b1feae620e2cec
HarryCaveMan <harris.joseph.088@gmail.com>
2024-03-16 18:29:04 -0400

    adding license copy in builder

--- src/bert/bert_reranking/builder.rs
+++ src/bert/bert_reranking/builder.rs
@@ -1,3 +1,24 @@
+// Copyright 2019-present, the HuggingFace Inc. team, The Google AI Language Team and Facebook, Inc.
+// Copyright 2019-2020 Guillaume Becquin
+// Copyright 2020 Maarten van Gompel
+// Copyright 2024 Harris Joseph
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+//
+//
+// # query/result reranking (via n-label paired sequence classification)
+// More generic sequence classification pipeline, works with multiple models (Bert, Roberta)
+// Private module used mostly by the bert_reranking::model module. Builder for bert_reranking_model::RerankingModel
+
 use std::path::{PathBuf};
 use serde::{Deserialize};
 use tch::Device;
